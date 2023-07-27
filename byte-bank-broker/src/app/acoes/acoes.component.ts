24a75affb4602688685d5ba7db76c07da5b77855
Victor Prado <victor0122@hotmail.com>
2023-07-26 21:41:52 -0300

    teste

---
+++ byte-bank-broker/src/app/acoes/acoes.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Acoes } from './modelo/acoes';
+import { AcoesService } from './acoes.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-acoes',
+  templateUrl: './acoes.component.html',
+  styleUrls: ['./acoes.component.css'],
+})
+export class AcoesComponent {
+  acoesInput = new FormControl();
+  acoes$ = this.acoesService.getAcoes();
+
+  constructor(private acoesService: AcoesService) {}
+}
